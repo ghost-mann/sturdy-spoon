cca38737a437bc52d6629b66da064d6050a49d08
ghost-mann <iamaob8@protonmail.com>
2025-04-29 15:28:31 +0300

    feat: added current time in index method in app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    # current date and time
+    now = datetime.datetime.now()
+
+    # format as HH:MM:SS
+    current_time = now.strftime("%H:%M:%S")
+
+    print(current_time)
