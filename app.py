0f25490b703146e7d8847a5d219a4b61c775ea09
ghost-mann <iamaob8@protonmail.com>
2025-04-29 16:12:18 +0300

    feat: added todo list in app.py and index.html template

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import datetime
 app = Flask(__name__)
 
-@app.route('/')
+todo_items = []
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # current date and time
     now = datetime.datetime.now()
 
     # format as HH:MM
     current_time = now.strftime("%Y-%m-%d %H:%M")
+    # add tasks and refresh
+    if request.method == 'POST':
+        task = request.form.get('task')
+        if task:
+            todo_items.append(task)
+        redirect('/')
 
-    return render_template('index.html', current_time=current_time)
+
+    return render_template('index.html', current_time=current_time, todo_items=todo_items)
 
 if __name__ == '__main__':
     app.run(debug=True)
